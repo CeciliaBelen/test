eb1960fcfd8cffb38d75557f2ca05a0b0973a839
T11641.Training11 <T11641.Training11@accenture.com>
2019-05-27 22:06:01 -0300

    Agrego archivos sobre eventos

--- proyecto/script.js
+++ proyecto/script.js
@@ -1,59 +1,21 @@
-let nombre = "Cecilia"
-let apellido = "Bascher"
-let nombre_completo = nombre + " " + apellido
-let numeros = [10,7,5,2,14,"Cecilia",true]
-numeros.push("Hola")
-numeros.push(45)
-let persona = {
-    nombre : "Cecilia",
-    edad : 29,
-    vive : true
-}
-//persona.trabaja = false
-//persona.tienePerrito = 0
-for (let index = 0; index < numeros.length; index++) {
-    const element = numeros[index];
-    console.log(numeros[index])
-}
-for (let prop in persona){
-    console.log(persona[prop])
-}
-console.clear()
+/* let btn = document.querySelector("button")
+btn.addEventListener("click", hacerClick)
+function hacerClick(){
+    console.log("Click!")
+} */
 
-/* let dos = document.getElementById("item2")
-dos.innerText = "Nuevo item"
-let items = document.getElementsByClassName("item")
-let lista = document.getElementsByTagName("li") */
-let dos = document.querySelector("#item2")
-let items = document.querySelectorAll(".item")
-let lista = document.querySelectorAll("li")
-//console.dir(lista)
-
-let p = document.createElement("p")
-p.innerText = "Mi nuevo texto"
-p.id = "nuevo"
-p.className = "parrafo"
-
-console.log(p)
-//console.log(persona)
 
 let body = document.querySelector("body")
-body.appendChild(p)
-
-/*Ejercicio:*/
-let ul = document.querySelector("ul")
-let item1 = document.querySelector("#item1")
-let li = document.createElement("li")
-li.innerText = "Un texto"
-ul.insertBefore(li, item1)
-console.log(ul)
- //Ejercicio 2:
-let nuevaLista = document.createElement("ul")
-for (let i = 0; i < 5; i++) {
-    //const element = nuevaLista[i];
-    let elemLista = document.createElement("li")
-    elemLista.innerText = "Nuevo texto " + i
-    nuevaLista.appendChild(elemLista)
+let btn = document.querySelector("button")
+let click = 1
+function hacerClick() {
+    let btnNuevo = document.createElement("button")
+    btnNuevo.innerText = "Click " + click++
+    body.appendChild(btnNuevo)
+    btnNuevo.addEventListener("click", funcionParaLosBotones)
+    console.log("Clicks")
 }
-body.appendChild(nuevaLista)
-console.log(nuevaLista)
+btn.addEventListener("click", hacerClick)
+function funcionParaLosBotones(){
+    console.log("Q de clicks: ")
+}
